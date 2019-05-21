c4ca639b309e8d383e73e2fac9275bc16c03745a
lious.q.k@gmail.com <lious.q.k@gmail.com>
2019-05-21 08:21:33 +0800

    1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,15 +1,20 @@
 <?php
+
 namespace app\index\controller;
 
 class Index
 {
     public function index()
     {
-        return 'Hello';
+        return 'Hello World';
     }
 
     public function hello($name = 'ThinkPHP5')
     {
         return 'hello,' . $name;
+    }
+    public function demoa($name)
+    {
+        return '你好' .$name;
     }
 }
